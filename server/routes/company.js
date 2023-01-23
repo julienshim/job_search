042325fb2907aa6b8396cad0cc25fb8c75b9ca9c
julienshim <25723428+julienshim@users.noreply.github.com>
2023-01-22 21:23:41 -0800

    companies route

--- server/routes/company.js
+++ server/routes/company.js
@@ -1,16 +1,10 @@
 const router = require('express').Router();
+const paginatedResults = require('../middlewares/paginatedResults');
 
 const pool = require('../config/keys');
 
-router.get('/', async (req, res) => {
-    try {
-        const allCompanies = await pool.query(
-            'SELECT * FROM company ORDER BY company_name;'
-        )
-        res.send(allCompanies)
-    } catch (err) {
-        res.sendStatus(500)
-    }
-})
+router.get('/', paginatedResults('companies'), async (req, res) => {
+    res.json(res.paginatedResults);
+  });
 
 module.exports = router;
