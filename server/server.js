7672c38de8d0bcfad01ca3e6a10743db74461310
julienshim <25723428+julienshim@users.noreply.github.com>
2023-01-23 21:32:47 -0800

    serverjs

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+// const cookieParser = require('cookie-parser');
 
-require('dotenv').config();
+require("dotenv").config();
 const { EPORT } = process.env;
 
 const app = express();
 const PORT = EPORT || 3000;
 
+app.use(
+  cors({
+    origin: `http://localhost:3000`,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
-const companyRouter = require('./routes/company');
-app.use('/company', companyRouter)
-
+const companyRouter = require("./routes/company");
+app.use("/company", companyRouter);
 
 app.listen(PORT, () => {
-    console.log(`App is now listening for request on http://localhost:${PORT}`);
-})
+  console.log(`App is now listening for request on http://localhost:${PORT}`);
+});
