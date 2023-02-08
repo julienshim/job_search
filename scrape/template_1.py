7b5cb1ff98a8be87ee635e503e9734025b87cf17
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-07 21:27:28 -0800

    pitpji

--- scrape/template_1.py
+++ scrape/template_1.py
@@ -16,3 +16,27 @@ def get_soup(page_number):
     response = request('GET', url, headers=headers, data=payload)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
+
+def parse_inline_text(inline_text):
+    return inline_text.string
+
+def parse_job_item(job_item):
+    job_title = job_item.find('h3', class_='job-item-title').string
+    job_link = ''
+    job_reference = job_item.find('input', class_='jetboost-list-item')['value']
+    inline_text = list(map(parse_inline_text, job_item.find_all('div', class_='inline-text')))
+    [city, seperator, state] = inline_text[0:3]
+    remote_possible = job_item.find('div', class_='remote-slug w-condition-invisible') is None
+    employment_type = inline_text[-1]
+
+    job_item_dict = {
+        'job_title': job_title.strip(),
+        'job_link': job_link.strip(),
+        'job_reference_no': job_reference.strip(),
+        'city': city.strip(),
+        'state': state.strip(),
+        'remote_possible': remote_possible,
+        'employment_type': employment_type.strip()
+    }
+    
+    return job_item_dict
