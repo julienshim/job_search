948fa93be5d9e2774d2d1f6ec1f12252f445b9d3
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-17 21:39:09 -0800

    gjrdlpij

--- data/template_2.py
+++ data/template_2.py
@@ -75,3 +75,19 @@ def fetch_all_jobs():
             sleep(rand_num)
 
     return tmp
+
+def get_job_ref_date(row):
+    pass
+    [import_date, job_reference_no, job_title, department, location, job_posting_url, status, notes] = row
+    tmp = {
+        'job_reference_no': job_reference_no,
+        'import_date': import_date
+    }
+    return tmp
+
+
+def load_previously_imported_jobs():
+    with open('') as ref:
+        data = [row for row in reader(ref)][1:]
+        data = list(map(get_job_ref_date, data))
+        return data
