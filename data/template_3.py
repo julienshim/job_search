f351bed595844583ec47c8d084585d0ddd01e5f4
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-20 21:44:04 -0800

    gs

---
+++ data/template_3.py
@@ -0,0 +1,18 @@
+from requests import request
+from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
+from csv import writer, reader
+from datetime import datetime
+from os import path
+
+
+def get_soup(page_number):
+    url = f""
+
+    payload={}
+    headers = {}
+
+    response = request('GET', url, headers=headers, data=payload)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
