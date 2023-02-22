0d0f69a7e70a8c3d22331c4382417e7bdb4a948c
julienshim <25723428+julienshim@users.noreply.github.com>
2023-02-21 21:44:49 -0800

    pitpji

--- data/template_3.py
+++ data/template_3.py
@@ -16,3 +16,23 @@ def get_soup(page_number):
     response = request('GET', url, headers=headers, data=payload)
     soup = BeautifulSoup(response.text, 'html.parser')
     return soup
+
+def parse_inline_text(inline_text):
+    return inline_text.string
+
+def parse_job_item(job_item):
+    job_link = f""
+    job_reference = job_item['href'].split('/')[2]
+    job_title = job_item.find('h2', class_='h6').string
+    location = job_item.find('span', class_='location').string
+    remote_possible = job_item.find('span', class_='remote') is not None
+    is_tagged_new = job_item.find('span', id=f'new-label-{job_reference}') is not None
+    job_item_dict = {
+        'job_title': job_title.strip(),
+        'job_link': job_link.strip(),
+        'job_reference': job_reference.strip(),
+        'location': location.strip(),
+        'remote_possible': remote_possible,
+        'is_tagged_new': is_tagged_new
+    }
+    return job_item_dict
