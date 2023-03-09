0cf8336785697e0d6bc0677d5697bcae72ce0c90
julienshim <25723428+julienshim@users.noreply.github.com>
2023-03-08 21:42:41 -0800

    fj

---
+++ data/template_4.py
@@ -0,0 +1,19 @@
+from csv import reader, writer
+from re import sub
+from requests import request
+from json import loads
+from time import sleep
+from random import randint
+from datetime import datetime
+from os import path
+
+def fetch_jobs(url):
+    payload = ""
+    headers = {}
+    response = request("GET", url, headers=headers, data=payload)
+    response_json = loads(response.text)
+
+    if 'result' in response_json:
+        jobs_found = response_json['result']
+        return jobs_found
+    return []
