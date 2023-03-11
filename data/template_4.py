e013cf43d524944e211499001beee120909168a1
julienshim <25723428+julienshim@users.noreply.github.com>
2023-03-10 21:48:01 -0800

    fi

--- data/template_4.py
+++ data/template_4.py
@@ -25,3 +25,23 @@ def fsb():
         data_body = data[1:]
         return data_body
 
+def fetch_items():
+    sb = fsb()
+    tmp = {}
+
+    for row in sb:
+        [company_id, url, company_name, careers_page, api] = list(map(lambda x: x.strip(), row))
+        if api in ['']:
+            api_url = careers_page
+            response_jobs = fetch_jobs(api_url)
+            response_jobs_len = len(response_jobs)
+
+            if response_jobs_len > 0:
+                print(f'Fetching {company_name} jobs... {response_jobs_len} jobs found')
+                if company_name in tmp:
+                    tmp[company_name] += response_jobs
+                else:
+                    tmp[company_name] = response_jobs
+            random_int = randint(3,7)
+            sleep(random_int)
+    return tmp
