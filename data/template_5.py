7f95b4e656c614684483a7e374747fb03de22096
julienshim <25723428+julienshim@users.noreply.github.com>
2023-05-07 23:16:09 -0700

    fetch_jobs

--- data/template_5.py
+++ data/template_5.py
@@ -6,3 +6,14 @@ from time import sleep
 from random import randint
 from datetime import datetime
 from os import path
+
+def fetch_jobs(url):
+    payload = ""
+    headers = {}
+    response = request("GET", url, headers=headers, data=payload)
+    response_json = loads(response.text)
+
+    if 'jobs' in response_json:
+        jobs_found = response_json['jobs']
+        return jobs_found
+    return []
