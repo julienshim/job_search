93d7641f1437e373e1b7e9a35a02e897565c8d9f
julienshim <25723428+julienshim@users.noreply.github.com>
2023-05-22 00:09:27 -0700

    init

---
+++ data/template_6.py
@@ -0,0 +1,8 @@
+from csv import reader, writer
+from re import sub
+from requests import request
+from json import loads
+from time import sleep
+from random import randint
+from datetime import datetime
+from os import path
