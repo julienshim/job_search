50c38b70edfa06cf49dc331f2505607357563c8c
julienshim <25723428+julienshim@users.noreply.github.com>
2023-05-25 00:14:37 -0700

    fetch_all_jobs

--- data/template_6.py
+++ data/template_6.py
@@ -24,3 +24,39 @@ def fetch_seed_body():
         data_header = data[0]
         data_body = data[1:]
         return data_body
+    
+def fetch_all_jobs():
+    reference = {
+        '': {
+            '',
+            ''
+        },
+        '': {
+            '',
+            ''
+        }
+    }
+
+seed_body = fetch_seed_body()
+tmp = {}
+for row in seed_body:
+    [company_id, url, company_name, careers_page, api] = list(map(lambda x: x.strip(), row))
+    if company_id in ['']:
+        api_url = ''
+        if company_id in ['']:
+            api_url = careers_page
+        elif api in ['']:
+            board_token = careers_page.replace(reference[api]['')
+            api_url = reference[api]['', board_token)
+        response_jobs = fetch_jobs(api_url)
+        response_jobs_len = len(response_jobs)
+
+        if response_jobs_len > 0:
+            print(f'')
+            if company_name in tmp:
+                tmp[company_name] += response_jobs
+            else:
+                tmp[company_name] = response_jobs
+        random_int = randint(3,7)
+        sleep(random_int)
+return tmp
