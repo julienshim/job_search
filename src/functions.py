f1272df93479866775ca00cb2beccadfe191a0f8
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-03 02:39:18 -0700

    fsb

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 from requests import request
 from json import loads, dumps
+from csv import reader
 from time import sleep
 from random import randint
 
@@ -13,3 +14,10 @@ def fetch_jobs(url, company_id):
         outfile.write(dumps(response_json, indent=4))
 
     sleep(randint(3,7))
+
+def fetch_seed_body(filter_value):
+    with open('./csv/seed.csv') as csv_input:
+        data = [row for row in reader(csv_input)]
+        data_body = data[1:]
+        data_body = list(filter(lambda x: filter_value in x[4].lower(), data_body))
+        return data_body
