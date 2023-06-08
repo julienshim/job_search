303e5e52c39bfc9adb5a1b12757031b8a43d324d
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-08 02:48:30 -0700

    faj

--- typ_1.py
+++ typ_1.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from requests import request
+from random import randint
+from time import sleep
 
 def get_date_n_weeks_ago():
     today = datetime.now()
@@ -42,3 +44,25 @@ def parse_job_item(job_item):
         'location': location
     }
     return tmp
+
+def fetch_all_jobs():
+    running = True
+    current_page_number = 1
+
+    while running:
+        soup = get_soup(current_page_number)
+        job_items = soup.find_all('div', class_='job-item')
+        job_items_parsed = list(map(parse_job_item, job_items))
+        job_items_parsed_len = len(job_items_parsed)
+        if job_items_parsed_len == 0:
+            running = False
+        else:
+            for job_item in job_items_parsed:
+                if job_item['location']:
+                    print(job_item['job_link'])
+                    get_seconds(job_item['job_link'])
+                    sleep(randint(3, 7))
+            current_page_number +=1
+            sleep(randint(3,7))
+
+fetch_all_jobs()
