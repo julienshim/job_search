20f082ec633b214e6309ebe67059e93c6bf23ed7
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-11 02:55:30 -0700

    gs

--- typ_2.py
+++ typ_2.py
@@ -1,7 +1,20 @@
 from datetime import datetime, timedelta
+from requests import request
+from bs4 import BeautifulSoup
 
 def get_date_n_weeks_ago():
     today = datetime.now()
     delta = timedelta(weeks=1)
     n_weeks_ago = today - delta
     return n_weeks_ago
+
+def get_soup(page_number):
+    print(f'Fetching Page {page_number} jobs...')
+    url = f'https://unamed.com/page/{page_number}'
+
+    payload={}
+    headers={}
+
+    response = request('GET', url, headers=headers, data=payload)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
