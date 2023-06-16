5a4ed4e79e1977d89c07b3c5f5f2bebb7bded8fd
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-16 03:01:07 -0700

    typ_3.py

---
+++ typ_3.py
@@ -0,0 +1,7 @@
+from datetime import datetime, timedelta
+
+def get_date_n_weeks_ago():
+    today = datetime.now()
+    delta = timedelta(weeks=1)
+    n_weeks_ago = today - delta
+    return n_weeks_ago
