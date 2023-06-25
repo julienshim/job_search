57556b137881a90c8708dfb70d3946db3e367a45
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-25 03:11:43 -0700

    fsb

--- typ_5.py
+++ typ_5.py
@@ -3,6 +3,7 @@ from requests import request
 from json import loads, dumps
 from random import randint
 from time import sleep
+from csv import reader
 
 def get_date_n_weeks_ago():
     today = datetime.now()
@@ -21,3 +22,10 @@ def fetch_jobs(url, company_id):
         outfile.write(dumps(response_json, indent=4))
 
     sleep(randint(3,7))
+
+def fetch_seed_body():
+    with open('./csv/seed.csv') as csv_input:
+        data = [row for row in reader(csv_input)]
+        data_header = data[0]
+        data_body = data[1:]
+        return data_body
