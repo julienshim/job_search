8dfcfa6125e4ee0b2ed088c1e2b6204d58b16bf6
julienshim <25723428+julienshim@users.noreply.github.com>
2023-06-28 03:15:05 -0700

    fj

--- typ_6.py
+++ typ_6.py
@@ -1,7 +1,23 @@
 from datetime import datetime, timedelta
+from requests import request
+from json import loads, dumps
+from random import randint
+from time import sleep
 
 def get_date_n_weeks_ago():
     today = datetime.now()
     delta = timedelta(weeks=1)
     n_weeks_ago = today - delta
     return n_weeks_ago
+
+def fetch_jobs(url, company_id):
+    print(company_id)
+    payload = ""
+    headers = {}
+    response = request("GET", url, headers=headers, data=payload)
+    response_json = loads(response.text)
+
+    with open(f'./json/unamed/{company_id}.json', 'w') as outfile:
+        outfile.write(dumps(response_json, indent=4))
+
+    sleep(randint(3,7))
